0deacceb9e1e9ece1f19295bca888bd2c59a9cae
cheap-glitch <cheap.glitch@gmail.com>
2019-12-18 16:23:45 +0100

    Add testing sample

---
+++ docs/sample.v
@@ -0,0 +1,19 @@
+module mymodule
+
+import os
+import gg
+import math
+
+
+
+/**
+ * Multiline comment
+ * TODO   todo mark
+ * FIXME  fixme mark
+ *
+ * "Strings" should be 'ignored' in comments
+ */
+
+// Single-line comment
+// TODO FIXME
+// "string" 'string'
